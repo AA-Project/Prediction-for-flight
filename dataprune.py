b78f4f9bbdff006f61946af9aa59abf400248be5
Derek Wang <58099268+DRKWang@users.noreply.github.com>
2022-03-22 15:59:10 -0400

    Update dataprune.py

--- dataprune.py
+++ dataprune.py
@@ -30,15 +30,14 @@ end_date = datetime.date(2020, 10, 1)
 time_between_dates = end_date - start_date
 days_between_dates = time_between_dates.days
 
-output = {}
-counter = 0
+#counter = 0
 
-print('output_{}-{}_predict_in_{}mins.dat'.format(start_date,end_date,60))
 for i in range(days_between_dates):
+    output = {}
     current_date = start_date + datetime.timedelta(days=i)
     str_date = "{}{:0>2}{:0>2}".format(current_date.year, current_date.month, current_date.day)
     # real result
-    for cur_hour in range(0):
+    for cur_hour in range(24):
         file_name = "hrrr.{}/conus/hrrr.t{:0>2}z.wrfsubhf00.grib2".format(str_date, cur_hour)
         try:
             download(file_name, cur_location + "/00.grib2")
@@ -51,12 +50,12 @@ for i in range(days_between_dates):
         grb = grbs()[idx_cloudtop - 1]
         mat = grb.values
         mmat = mat[lat_range[0]:lat_range[1], long_range[0]: long_range[1]]
-        output["{}_{:0>2}_cloudtop_real".format(str_date, cur_hour)] = mmat
+        output["{:0>2}_clock_cloudtop_real".format(cur_hour)] = mmat
 
         grb = grbs()[idx_VIL - 1]
         mat = grb.values
         mmat = mat[lat_range[0]:lat_range[1], long_range[0]: long_range[1]]
-        output["{}_{:0>2}_VIL_real".format(str_date, cur_hour)] = mmat
+        output["{:0>2}_clock_VIL_real".format(cur_hour)] = mmat
         os.remove(cur_location + "/00.grib2")
 
     # predict result
@@ -74,15 +73,13 @@ for i in range(days_between_dates):
         grb = grbs()[idx_cloudtop - 1 + multiple * idx_cycle]
         mat = grb.values
         mmat = mat[lat_range[0]:lat_range[1], long_range[0]: long_range[1]]
-        output["{}_{:0>2}_cloudtop_predict".format(str_date, cur_hour)] = mmat
+        output["{:0>2}_clock_cloudtop_{}_predict".format(cur_hour, delta_time)] = mmat
 
         grb = grbs()[idx_VIL - 1 + multiple * idx_cycle]
         mat = grb.values
         mmat = mat[lat_range[0]:lat_range[1], long_range[0]: long_range[1]]
-        output["{}_{:0>2}_VIL_predict".format(str_date, cur_hour)] = mmat
+        output["{:0>2}_clock_VIL_{}_predict".format(cur_hour, delta_time)] = mmat
         os.remove(cur_location + "/00.grib2")
-        counter +=1
-if counter % 30 == 0:
-    with open('output_{}-{}_predict_in_{}mins.dat'.format(start_date,end_date,delta_time), 'wb') as f:
+        #counter +=1
+    with open('output_{}.dat'.format(current_date), 'wb') as f:
         pickle.dump(output, f)
-print('output_{}-{}_predict_in_{}mins.dat'.format(start_date,end_date,delta_time))
